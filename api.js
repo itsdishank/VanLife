75c06d9aea8fc013ec5a9699a8e1b2af1e5cdb16
itsdishank <dishuhanwate007@gmail.com>
2026-02-18 12:42:55 -0500

    refactor: update package.json and improve API calls
    - Changed project name and description in package.json.
    - Updated dependencies: added firebase and updated react-router-dom and miragejs.
    - Refactored HostVanDetail and VanDetail components to use getVan instead of getHostVans and getVans.
    - Updated server.js to allow passthrough for Firestore API requests.

--- api.js
+++ api.js
@@ -1,30 +1,96 @@
-export async function getVans(id) {
-    const url = id ? `/api/vans/${id}` : "/api/vans"
-    const res = await fetch(url)
-    if (!res.ok) {
-        throw {
-            message: "Failed to fetch vans",
-            statusText: res.statusText,
-            status: res.status
-        }
-    }
-    const data = await res.json()
-    return data.vans
+import { initializeApp } from "firebase/app"
+import {
+    getFirestore,
+    collection,
+    doc,
+    getDocs,
+    getDoc,
+    query,
+    where,
+    documentId
+} from "firebase/firestore/lite"
+
+const firebaseConfig = {
+    apiKey: "AIzaSyD_k3v3HK3tKEqhlqFHPkwogW7PqEqhGhk",
+    authDomain: "vanlife-a1af5.firebaseapp.com",
+    projectId: "vanlife-a1af5",
+    storageBucket: "vanlife-a1af5.appspot.com",
+    messagingSenderId: "803007000356",
+    appId: "1:803007000356:web:446cd3a1ca406839258db1"
+};
+
+// const firebaseConfig = {
+//   apiKey: "AIzaSyAjg8QMgM-zE52SJM9M-O162-FhoHnmZME",
+//   authDomain: "vanlife-834c2.firebaseapp.com",
+//   projectId: "vanlife-834c2",
+//   storageBucket: "vanlife-834c2.firebasestorage.app",
+//   messagingSenderId: "1075339603658",
+//   appId: "1:1075339603658:web:768d142ea2ab74e11a0e33"
+// };
+
+const app = initializeApp(firebaseConfig)
+const db = getFirestore(app)
+
+// Refactoring the fetching functions below
+const vansCollectionRef = collection(db, "vans")
+
+export async function getVans() {
+    const snapshot = await getDocs(vansCollectionRef)
+    const vans = snapshot.docs.map(doc => ({
+        ...doc.data(),
+        id: doc.id
+    }))
+    return vans
 }
 
-export async function getHostVans(id) {
-    const url = id ? `/api/host/vans/${id}` : "/api/host/vans"
-    const res = await fetch(url)
-    if (!res.ok) {
-        throw {
-            message: "Failed to fetch vans",
-            statusText: res.statusText,
-            status: res.status
-        }
+export async function getVan(id) {
+    const docRef = doc(db, "vans", id)
+    const snapshot = await getDoc(docRef)
+    return {
+        ...snapshot.data(),
+        id: snapshot.id
     }
-    const data = await res.json()
-    return data.vans
 }
+
+export async function getHostVans() {
+    const q = query(vansCollectionRef, where("hostId", "==", "123"))
+    const snapshot = await getDocs(q)
+    const vans = snapshot.docs.map(doc => ({
+        ...doc.data(),
+        id: doc.id
+    }))
+    return vans
+}
+
+/* 
+This 👇 isn't normally something you'd need to do. Instead, you'd 
+set up Firebase security rules so only the currently logged-in user 
+could edit their vans.
+
+https://firebase.google.com/docs/rules
+
+I'm just leaving this here for educational purposes, as it took
+me a while to find the `documentId()` function that allows you
+to use a where() filter on a document's ID property. (Since normally
+it only looks at the data() properties of the document, meaning you
+can't do `where("id", "==", id))`
+
+It also shows how you can chain together multiple `where` filter calls
+*/
+
+// export async function getHostVan(id) {
+//     const q = query(
+//         vansCollectionRef,
+//         where(documentId(), "==", id),
+//         where("hostId", "==", "123")
+//     )
+//     const snapshot = await getDocs(q)
+//     const vans = snapshot.docs.map(doc => ({
+//         ...doc.data(),
+//         id: doc.id
+//     }))
+//     return vans[0]
+// }
 
 export async function loginUser(creds) {
     const res = await fetch("/api/login",
